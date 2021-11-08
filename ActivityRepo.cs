b8f8dcc27ca775dac005393056a3cad80e92352d
Lori Kunzler <lorimk70@gmail.com>
2021-11-08 17:50:20 -0500

    activity repo change

--- ActivityRepo.cs
+++ ActivityRepo.cs
@@ -4,26 +4,36 @@ using System.Text;
 
 namespace LouisvilleFun
 {
-    class ActivityRepo
+    public class ActivityRepo
     {
-        public static Dictionary<string, Activity>InitializeActivity()
+        public ActivityRepo()
         {
-        Activity activity1 = new Activity("Idlewild", 10.00, "All ages");
-        Activity activity2 = new Activity("Thirsty Pedaler", 70.00, "21+");
-        Activity activity3 = new Activity("Activate Games", 25.00, "10+");
-        Activity activity4 = new Activity("Slugger Museum", 16.00, "All ages");
-        Activity activity5 = new Activity("Flying Axes", 20.00, "13+");
-        
-        
-        
-        Activity[] activites = new Activity[4];
+            ActivityDictionary = new Dictionary<int, Activity>();
 
-        activites[0] = activity1;
-            activites[1] = activity2;
-            activites[2] = activity3;
-            activites[3] = activity4;
-            activites[4] = activity5;
 
+            Activity activity1 = new Activity("Idlewild", 10.00, "All ages");
+            Activity activity2 = new Activity("Thirsty Pedaler", 70.00, "21+");
+            Activity activity3 = new Activity("Activate Games", 25.00, "10+");
+            Activity activity4 = new Activity("Slugger Museum", 16.00, "All ages");
+            Activity activity5 = new Activity("Flying Axes", 20.00, "13+");
+            MoreActivities activity6 = new Activity("Louisville Zoo", 10.50, "All ages", "Food Available");
+            MoreActivities activity7 = new Activity("SLugger Museum", 16.00, "All ages", "Food Available");
+
+
+
+
+        }
+
+        public IDictionary<int, Activity> ActivityDictionary { get; private set; }
+
+
+
+    }
+
+}           
+        
+        
+        
 
     }
 }
